51a4c683152c7472956c09014ef0615b4f804007
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2021-07-26 14:08:07 -0300

    criando objetos da classe Video e fazendo testes

--- PHP/ProjetoYouTube/index.php
+++ PHP/ProjetoYouTube/index.php
@@ -1,3 +1,11 @@
+<pre>
 <?php
+require "Video.php";
+// $v = array();
+$v[0] = new Video("Aula 1 de POO");
+$v[1] = new Video("Aula 12 de PPH");
+$v[2] = new Video("Aula 15 de HTML5");
 
+print_r($v);
 ?>
+</pre>
