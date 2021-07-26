cedb49734d5f16059fa949e88c3be75019b83504
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2021-07-26 14:51:40 -0300

    criando objetos da classe Gafanhoto.php e fazendo testes

--- PHP/ProjetoYouTube/index.php
+++ PHP/ProjetoYouTube/index.php
@@ -1,11 +1,16 @@
 <pre>
 <?php
 require "Video.php";
+require "Gafanhoto.php";
 // $v = array();
 $v[0] = new Video("Aula 1 de POO");
 $v[1] = new Video("Aula 12 de PPH");
 $v[2] = new Video("Aula 15 de HTML5");
 
 print_r($v);
+
+$user[0] = new Gafanhoto("Will", 31, "M", "willferr");
+$user[1] = new Gafanhoto("carlos", 31, "M", "carlferr");
+print_r($user);
 ?>
 </pre>
