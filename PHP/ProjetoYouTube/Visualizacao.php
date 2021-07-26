8c4c8c59056b00264bcc2db3a844dc9b3b6207ff
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2021-07-26 17:15:28 -0300

    add os metodos avaliar

--- PHP/ProjetoYouTube/Visualizacao.php
+++ PHP/ProjetoYouTube/Visualizacao.php
@@ -1,6 +1,6 @@
 <?php
-require "Video.php";
-require "Gafanhoto.php";
+require_once "Video.php";
+require_once "Gafanhoto.php";
 class Visualizacao{
     private $espectador;
     private $filme;
@@ -9,6 +9,8 @@ class Visualizacao{
     public function __construct($espectador, $filme){
         $this->espectador = $espectador;
         $this->filme = $filme;
+        $this->filme->setViews($this->filme->getViews() + 1);
+        $this->espectador->setTotAssis($this->espectador->getTotAssis() + 1);
     }
     public function setEspectador($espec){
         $this->espectador = $espec;
@@ -21,6 +23,27 @@ class Visualizacao{
     }
     public function getFilme(){
         return $this->filme;
+    }
+
+    #Métodos simples
+    public function avaliar(){
+        $this->filme->setAva(5);
+    }
+    public function avaliarNota($nota){
+        $this->filme->setAva($nota);
+    }
+    public function avaliarPor($por){
+        $nova = 0;
+        if($por <= 20){
+            $nova = 3;
+        }else if($por <= 50){
+            $nova = 5;
+        }else if($por <= 80){
+            $nova = 8;
+        }else{
+            $nova = 10;
+        }
+        $this->filme->setAva($nova);
     }
 }
 ?>
