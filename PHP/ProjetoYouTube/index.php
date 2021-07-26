15cb4563135c8fe406f4ed6069558b3c683ea22a
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2021-07-26 17:16:25 -0300

    PHP/POO finalizando o projetoYouTube - aula 15b - Curso em video

--- PHP/ProjetoYouTube/index.php
+++ PHP/ProjetoYouTube/index.php
@@ -1,16 +1,23 @@
 <pre>
 <?php
-require "Video.php";
-require "Gafanhoto.php";
+require_once "Video.php";
+require_once "Gafanhoto.php";
+require_once "Visualizacao.php";
 // $v = array();
 $v[0] = new Video("Aula 1 de POO");
 $v[1] = new Video("Aula 12 de PPH");
 $v[2] = new Video("Aula 15 de HTML5");
-
 print_r($v);
 
 $user[0] = new Gafanhoto("Will", 31, "M", "willferr");
 $user[1] = new Gafanhoto("carlos", 31, "M", "carlferr");
 print_r($user);
+
+$vis[0] = new Visualizacao($user[0], $v[1]);
+$vis[1] = new Visualizacao($user[1], $v[1]);
+$vis[0]->avaliar();
+$vis[1]->avaliarPor(85);
+print_r($vis);
+
 ?>
 </pre>
